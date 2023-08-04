1e4d027dd618d2a97fae4bde44fe7e4ae987a4be
Alexander Carrillo <alexandercarrillomartinez8@gmail.com>
2023-08-03 20:52:33 -0500

    OK

---
+++ README.md
@@ -0,0 +1,4 @@
+## Yorman Alexander Carrillo Martinez
+
+## Para iniciar el servidor nodemon start
+## Server is running on PORT 3002
--- public/script.js
+++ public/script.js
@@ -34,63 +34,96 @@ async function loadDishes() {
 
             const select = document.getElementById('selectID')
             const option = document.createElement('option');
-            option.id = 'idOption' 
+            option.id = 'idOption'
             option.value = element._id;
             option.textContent = element.name;
             select.appendChild(option)
-            console.log(element._id)
         });
     } catch (error) {
         console.error('Error al onbtener los platos:', error.message);
     }
 }
 
-document.getElementById('filterForm').addEventListener('submit', async function(event){
-    event.preventDefault();
-    try {
-        const _id = document.getElementById('idOption')
-        const response = await fetch(URLAPI+_id )
-        if (!response.ok) {
-            throw new Error('La respuesta de la API no fue exitosa');
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('filterForm').addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const _id = document.getElementById('selectID').value;
+        try {
+            const response = await fetch(URLAPI + _id);
+
+            const tbody = document.getElementById('tbody');
+            while (tbody.firstChild) {
+                tbody.removeChild(tbody.firstChild);
+            }
+            if (!response.ok) {
+                throw new Error('La respuesta de la API no fue exitosa');
+            }
+            const data = await response.json();
+//            console.log(data);
+
+            const row = document.createElement('tr');
+            const colId = document.createElement('td');
+            colId.append(document.createTextNode(data.data.idDish));
+            const colName = document.createElement('td');
+            colName.append(document.createTextNode(data.data.name));
+            const colCalories = document.createElement('td');
+            colCalories.append(document.createTextNode(data.data.calories));
+            const colVegetarian = document.createElement('td');
+            colVegetarian.append(document.createTextNode(data.data.isVegetarian));
+            const colValue = document.createElement('td');
+            colValue.append(document.createTextNode(data.data.value));
+            const colComments = document.createElement('td');
+            colComments.append(document.createTextNode(data.data.comments));
+            row.append(colId);
+            row.append(colName);
+            row.append(colCalories);
+            row.append(colVegetarian);
+            row.append(colValue);
+            row.append(colComments);
+            document.getElementById('tbody').append(row);
+        } catch (error) {
+            console.error('Error al obtener los platos:', error.message);
         }
-        const data = await response.json()
-         console.log(data.data)
-    } catch (error) {
-        console.error('Error al onbtener los platos:', error.message);
-    }
-})
-
-
-document.getElementById('dishForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const id = document.getElementById('inputId').value;
-    const name = document.getElementById('inputName').value;
-    const calories = document.getElementById('inputCalories').value;
-    const isVegetarian = document.getElementById('selectVegetarian').value;
-    const value = document.getElementById('inputValue').value;
-    const description = document.getElementById('inputDescription').value;
-    console.log(id, name, calories, isVegetarian, value, description);
-
-    const dataDish = {
-        "idDish": id,
-        "name": name,
-        "calories": calories,
-        "isVegetarian": isVegetarian,
-        "value": value,
-        "comments": description,
-    }
-    addDish(dataDish)
+    });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('dishForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const id = document.getElementById('inputId').value;
+        const name = document.getElementById('inputName').value;
+        const calories = document.getElementById('inputCalories').value;
+        const isVegetarian = document.getElementById('selectVegetarian').value;
+        const value = document.getElementById('inputValue').value;
+        const description = document.getElementById('inputDescription').value;
+        console.log(id, name, calories, isVegetarian, value, description);
+
+        const dataDish = {
+            "idDish": id,
+            "name": name,
+            "calories": calories,
+            "isVegetarian": isVegetarian,
+            "value": value,
+            "comments": description,
+        }
+        addDish(dataDish)
+    })
+})
 
 async function addDish(dish) {
     try {
-        const response = await fetch(URLAPI,
-            {
+        const response = await fetch(URLAPI, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(dish)
             })
-        const responseData = response.json()
+        const responseData = await response.json()
+        console.log(responseData)
+        
+        if(responseData.state == false){
+            alert(responseData.data + " Cambie el id.")
+        }
 
         if (!response.ok) {
             throw new Error('Error al registrar el plato en la API. Estado de respuesta: ' + response.status);
